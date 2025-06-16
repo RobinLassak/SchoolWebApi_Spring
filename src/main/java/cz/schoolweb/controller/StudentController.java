0c6877716a791cc3ddab94b6a8ef4c97488e3b2c
Robin <lassak.robin@gmail.com>
2025-06-16 13:08:02 +0200

    get students

--- src/main/java/cz/schoolweb/controller/StudentController.java
+++ src/main/java/cz/schoolweb/controller/StudentController.java
@@ -1,10 +1,14 @@
 package cz.schoolweb.controller;
 
+import cz.schoolweb.dto.StudentDto;
+import cz.schoolweb.entity.StudentEntity;
 import cz.schoolweb.mapper.StudentMapper;
 import cz.schoolweb.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -16,5 +20,10 @@ public class StudentController {
     public StudentController(StudentService studentService, StudentMapper studentMapper) {
         this.studentService = studentService;
         this.studentMapper = studentMapper;
+    }
+    //Zobrazeni vsech studentu
+    @GetMapping({"/students", "/students/"})
+    public List<StudentDto> getStudents() {
+        return null;
     }
 }
--- src/main/java/cz/schoolweb/service/StudentService.java
+++ src/main/java/cz/schoolweb/service/StudentService.java
@@ -1,9 +1,14 @@
 package cz.schoolweb.service;
 
+import cz.schoolweb.dto.StudentDto;
+import cz.schoolweb.entity.StudentEntity;
 import cz.schoolweb.mapper.StudentMapper;
 import cz.schoolweb.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class StudentService {
@@ -14,5 +19,14 @@ public class StudentService {
     public StudentService(StudentMapper studentMapper, StudentRepository studentRepository) {
         this.studentMapper = studentMapper;
         this.studentRepository = studentRepository;
+    }
+    //Zobrazeni vsech studentu
+    public List<StudentDto> getStudents(){
+        List<StudentEntity> studentEntities = studentRepository.findAll();
+        List<StudentDto> studentDtos = new ArrayList<>();
+        for(StudentEntity studentEntity : studentEntities){
+            studentDtos.add(studentMapper.toDto(studentEntity));
+        }
+        return studentDtos;
     }
 }
