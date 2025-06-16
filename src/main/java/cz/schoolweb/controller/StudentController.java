1e3f13a9c4b421165caefacfb7ff453a89ffa835
Robin <lassak.robin@gmail.com>
2025-06-16 11:43:26 +0200

    student controller

--- src/main/java/cz/schoolweb/controller/StudentController.java
+++ src/main/java/cz/schoolweb/controller/StudentController.java
@@ -1,4 +1,20 @@
 package cz.schoolweb.controller;
 
+import cz.schoolweb.mapper.StudentMapper;
+import cz.schoolweb.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
 public class StudentController {
+    StudentService studentService;
+    StudentMapper studentMapper;
+
+    @Autowired
+    public StudentController(StudentService studentService, StudentMapper studentMapper) {
+        this.studentService = studentService;
+        this.studentMapper = studentMapper;
+    }
 }
