b104d08c2e42625ecb40c4735705c9cbb172a2bb
Robin <lassak.robin@gmail.com>
2025-06-16 11:41:57 +0200

    student service

---
+++ src/main/java/cz/schoolweb/controller/StudentController.java
@@ -0,0 +1,4 @@
+package cz.schoolweb.controller;
+
+public class StudentController {
+}
--- src/main/java/cz/schoolweb/service/StudentService.java
+++ src/main/java/cz/schoolweb/service/StudentService.java
@@ -1,4 +1,18 @@
 package cz.schoolweb.service;
 
+import cz.schoolweb.mapper.StudentMapper;
+import cz.schoolweb.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class StudentService {
+    StudentMapper studentMapper;
+    StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentMapper studentMapper, StudentRepository studentRepository) {
+        this.studentMapper = studentMapper;
+        this.studentRepository = studentRepository;
+    }
 }
