d00b67548077c8a74b3e3ebca9251b056fa5fe01
Robin <lassak.robin@gmail.com>
2025-06-16 11:13:39 +0200

    student DTO

--- src/main/java/cz/schoolweb/dto/StudentDto.java
+++ src/main/java/cz/schoolweb/dto/StudentDto.java
@@ -1,4 +1,15 @@
 package cz.schoolweb.dto;
 
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class StudentDto {
+    private int Id;
+    private String firstName;
+    private String lastName;
+    private LocalDate dateOfBirth;
 }
