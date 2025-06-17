2a17a7a609cf9e4b7d14f677feba47af4ba14812
Robin <lassak.robin@gmail.com>
2025-06-17 15:42:40 +0200

    Student entity - oprava

--- src/main/java/cz/schoolweb/entity/StudentEntity.java
+++ src/main/java/cz/schoolweb/entity/StudentEntity.java
@@ -16,7 +16,7 @@ public class StudentEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int Id;
+    private int id;
     private String firstName;
     private String lastName;
     private LocalDate dateOfBirth;
