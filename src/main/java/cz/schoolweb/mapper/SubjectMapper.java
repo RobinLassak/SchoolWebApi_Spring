d64146d6e03c49a98fcbe3805c25bd16c1e18376
Robin <lassak.robin@gmail.com>
2025-06-17 15:51:13 +0200

    Subject mapper

--- src/main/java/cz/schoolweb/mapper/SubjectMapper.java
+++ src/main/java/cz/schoolweb/mapper/SubjectMapper.java
@@ -1,4 +1,12 @@
 package cz.schoolweb.mapper;
 
+import cz.schoolweb.dto.SubjectDto;
+import cz.schoolweb.entity.SubjectEntity;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
 public interface SubjectMapper {
+
+    SubjectEntity toEntity(SubjectDto subjectDto);
+    SubjectDto toDto(SubjectEntity subjectEntity);
 }
