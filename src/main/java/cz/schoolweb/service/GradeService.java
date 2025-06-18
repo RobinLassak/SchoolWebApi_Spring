d7a6991f2f2466df32e84e0757b278fa0c5d8f5d
Robin <lassak.robin@gmail.com>
2025-06-18 09:37:29 +0200

    grade service - oprava

--- src/main/java/cz/schoolweb/service/GradeService.java
+++ src/main/java/cz/schoolweb/service/GradeService.java
@@ -2,6 +2,8 @@ package cz.schoolweb.service;
 
 import cz.schoolweb.mapper.GradeMapper;
 import cz.schoolweb.repository.GradeRepository;
+import cz.schoolweb.repository.StudentRepository;
+import cz.schoolweb.repository.SubjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,10 +12,15 @@ public class GradeService {
 
     GradeMapper gradeMapper;
     GradeRepository gradeRepository;
+    SubjectRepository subjectRepository;
+    StudentRepository studentRepository;
 
     @Autowired
-    public GradeService(GradeMapper gradeMapper, GradeRepository gradeRepository) {
+    public GradeService(GradeMapper gradeMapper, GradeRepository gradeRepository,
+                        SubjectRepository subjectRepository, StudentRepository studentRepository) {
         this.gradeMapper = gradeMapper;
         this.gradeRepository = gradeRepository;
+        this.subjectRepository = subjectRepository;
+        this.studentRepository = studentRepository;
     }
 }
