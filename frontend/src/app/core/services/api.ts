ffb59e09ae7fff47c39c088b9c80650a1c81e059
Robin <lassak.robin@gmail.com>
2025-11-05 16:00:20 +0100

    core service - createURL + toPromise + GET zaklad

--- frontend/src/app/core/services/api.ts
+++ frontend/src/app/core/services/api.ts
@@ -1,11 +1,15 @@
 import {DOCUMENT} from '@angular/common';
 import {Inject, Injectable} from '@angular/core';
+import {HttpClient, HttpResponse} from '@angular/common/http';
+import {firstValueFrom, map, Observable} from 'rxjs';
 
 @Injectable()
 export class ApiService {
   private _apiUrl: string = null;
 
-  constructor(@Inject(DOCUMENT) private _document: any) {
+  constructor(@Inject(DOCUMENT) private _document: any, private _http: HttpClient) {
+
+      //Poskladani api URL
       this._apiUrl = [
         _document.location.protocol,
         "//",
@@ -14,5 +18,28 @@ export class ApiService {
         _document.location.port,
         "/schoolapi"
       ].join('');
+  }
+
+  //Poskladani URL pro api requesty
+  createUrl(path: string): string{
+    if(!path){
+      throw new Error("Nelze volat API bez zadane cesty")
+    }
+    if(path[0] != "/"){
+      path = "/" + path
+    }
+    return this._apiUrl + path
+  }
+  //Convert Observable to Promise
+  private toPromise<T>(observable: Observable<T>): Promise<T> {
+    return firstValueFrom(observable);
+  }
+
+  //GET
+  get<T>(path: string, options?: any): Promise<T>{
+    let promise: Promise<T> = this.toPromise<T>(
+      this._http.get<T>(this.createUrl(path))
+    )
+    return promise
   }
 }
